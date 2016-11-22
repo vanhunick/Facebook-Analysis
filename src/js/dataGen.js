349f845df5c7a5e980aca8bb377d579ec6cb8a60
vanhunick <nickyvanhulst@gmail.com>
2016-11-23 11:07:36 +1300

    Created a simple data Genertor for testing

--- src/js/dataGen.js
+++ src/js/dataGen.js
@@ -1,16 +1,30 @@
-
-
-function createRandomMessage(length, ){
-
-}
+// Some random words
+var words = ["moldavia","eclipsing","minotaur","lido","halloween","semite","cylvia","tatiana",
+"degrease","ectype","elena","picara","dividedly","pottier","yawata","macu","muggur","forsythia",
+"bakeries","memphian","fubsiest","pygmalion","sotted","yap","dulcinea","revelry","cwo","bluey",
+"rifler","delilah","madrona","mulliken","bulldoze","eddie","cheney","dysuria","analogy","annwn",
+"dottrel","phocine","stoss","unveering","atiptoe","mandarin","weaponed","solver","defile","tumuluses",
+"ruminate","dresser","daira","blankbook","hague","macled","lowcased","macled","lowcased","weirdieblankbook"];
 
 var days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
 var months = ["January","Februaury","March","April","May","June","July","August","September","October","November","December"];
 
+// Some random names
+var names = ["Candy Maske","Faustino Regis","Newton Masson","Iris Lucier", "Cherilyn Meade", "Kelli Dyer",  
+"Debera Eisenhart","Beatris Adamek","Jacquetta Asberry","Herschel Whisler"];
+
+function createRandomMessage(){
+	let message = [];
+	for(let i = 0; i < getRandomInt(0,10); i++){ // Create a random length message
+		message.push(words[getRandomInt(0,words.length)]); // Grab a random word out of the words array
+	}
+	return message;
+}
 
 function createRandomTimeData(){
-	let day = days[getRandomInt(0,day.length)];
+	let day = days[getRandomInt(0,days.length)];
 	let month = months[getRandomInt(0,months.length)];
+	let monthDay = getRandomInt(1,32); 
 	let year = getRandomInt(2010,2016);
 
 	let hour = getRandomInt(1,13);
@@ -25,9 +39,18 @@ function createRandomTimeData(){
 		amString = "pm";
 	}
 
-	return new timeData(day,month,year, ""+hour + "." +min+""+amString, timeZ,am);
+	return new timeData(day,month,monthDay,year, ""+hour + "." +min+""+amString, timeZ,am);
 }
 
+
+function createRandomMessageData(){
+	let time = createRandomTimeData();
+	let message = createRandomMessage();
+	let sender = names[getRandomInt(0,names.length)];
+	let peopleInThread = [sender,names[getRandomInt(0,names.length)]]
+
+	return new messageData(sender,peopleInThread,time,message);
+}
 
 // Random number function max not unclusive
 function getRandomInt(min, max) {
@@ -35,3 +58,6 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+
+
