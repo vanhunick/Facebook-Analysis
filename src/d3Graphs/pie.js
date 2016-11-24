bf8a8a6cbeaefe1154afae270dd79fda9b6af616
vanhunick <nickyvanhulst@gmail.com>
2016-11-25 10:44:41 +1300

    Created pie graph showing messages recieved from top 10 friends

---
+++ src/d3Graphs/pie.js
@@ -0,0 +1,56 @@
+function showPie(dataSource) {
+    
+    var width = 400,
+        height = 400,
+        radius = Math.min(width, height) / 2;
+
+    var color = d3.scale.category10();
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius)
+        .innerRadius(0);
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function (d) { return d.count; });
+
+    let data = dataSource;
+
+    var svg = d3.select("#pie").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius)
+        .innerRadius(0);
+
+    var labelArc = d3.svg.arc()
+        .outerRadius(radius - 40)
+        .innerRadius(radius - 40);
+
+    var path = svg.selectAll('path')
+        .data(pie(data))
+        .enter()
+        .append('path')
+        .attr('d', arc)
+        .attr('fill', function (d) {
+            return color(d.data.name);
+        });
+
+    var g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+
+    g.append("text")
+        .attr("transform", function (d) { return "translate(" + labelArc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .text(function (d) { return d.data.name; });
+}
+
+function type(d) {
+    d.count = +d.count;
+    return d;
+}
