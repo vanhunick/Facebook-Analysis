73b73a633e9056cb261145dccd884f53dea40dc1
Nicky van Hulst <nickyvanhulst@gmail.com>
2016-11-30 09:35:57 +1300

    Started creating test framework

---
+++ src/test/dataProcessorTests.js
@@ -0,0 +1,14 @@
+var assert = chai.assert;
+
+
+describe('Array', function() {
+  it('should return nothing ', function() {
+
+    let messageArray = dataGen.get  
+
+    let dp = new DataProcessor();
+
+
+    assert.equal(arr.length, 0);
+  });
+});
