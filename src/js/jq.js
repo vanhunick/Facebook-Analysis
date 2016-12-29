bc25b9da5baf2200ad579f77668cc1456431d663
Nicky van Hulst <nickyvanhulst@gmail.com>
2016-12-30 08:51:04 +1300

    Commented file

--- src/js/jq.js
+++ src/js/jq.js
@@ -1,80 +1,71 @@
 $(document).ready(function(){
+    // Hides the loading bars
     $('#lf').hide();
     $('#ld').hide();
 
     $("#wordTableF").hide();
 
-
+    // Shows the loadings bars when the user clicks load file
     $('#files').click(function(){
         $('#lf').show();
         $('#ld').show();
     });
 
-    // Respond to search bar button being pressed
+    // Respond to search bar button being pressed for friend search
      $("#searchBut").click(function(){
          var val = $('#search-friends').val();
         friendSearched(val);
-    }); 
+    });
 
     // Respond to enter being pressed in search bar
     $('#search-friends').keypress(function(event){
-
-	var keycode = (event.keyCode ? event.keyCode : event.which);
-	if(keycode == '13'){
-        var val = $('#search-friends').val();
-        friendSearched(val);
-	}
-
+	     var keycode = (event.keyCode ? event.keyCode : event.which);
+	      if(keycode == '13'){
+          var val = $('#search-friends').val();
+          friendSearched(val);
+	       }
     });
 
-
     // Respond to search bar button being pressed for words
-     $("#searchButLang").click(function(){
-         var val = $('#search-words').val();
-        wordSearched(val);
-    }); 
-    
+    $("#searchButLang").click(function(){
+      var val = $('#search-words').val();
+      wordSearched(val);
+    });
 
     // Respond to enter being pressed in search bar for words
     $('#search-words').keypress(function(event){
+	     var keycode = (event.keyCode ? event.keyCode : event.which);
+	      if(keycode == '13'){
+          var val = $('#search-words').val();
+          wordSearched(val);
+	       }
+       });
 
-	var keycode = (event.keyCode ? event.keyCode : event.which);
-	if(keycode == '13'){
-        var val = $('#search-words').val();
-        wordSearched(val);
-	}
-
-    });
-
-    displayRandomData(); 
-
+       displayRandomData(); // Fills up the page wiuth example data
 });
 
 
 
   // setup autocomplete function pulling from friends[] array
   function updateAutocomplete(){
-$('#search-friends').autocomplete({
-    lookup: getFriends(),
-    onSelect: function (suggestion) {
-    
-    var thehtml = 'Selected friend : <span style="color: orangered;">' + suggestion.value;
-      
-      $('#search-output').html(thehtml);
-    }
-  });
+    $('#search-friends').autocomplete({
+      lookup: getFriends(),
+      onSelect: function (suggestion) {
+        var thehtml = 'Selected friend : <span style="color: orangered;">' + suggestion.value;
+        $('#search-output').html(thehtml);
+      }
+    });
   }
-  
+
+  // setup autocomplete function pulling from words[] array
   function updateWordAutocomplete(){
-$('#search-words').autocomplete({
-    lookup: getWords(),
-    onSelect: function (suggestion) {
-    
-    var thehtml = 'Selected Word : <span style="color: orangered;">' + suggestion.value;
-      
+    $('#search-words').autocomplete({
+      lookup: getWords(),
+      onSelect: function (suggestion) {
+        var thehtml = 'Selected Word : <span style="color: orangered;">' + suggestion.value;
       $('#search-output-word').html(thehtml);
-    }
-  });
+      }
+    });
   }
 
 updateWordAutocomplete();
